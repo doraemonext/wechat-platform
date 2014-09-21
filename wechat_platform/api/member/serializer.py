032d1de59825758e948f753aee56892f99410483
doraemonext <doraemonext@gmail.com>
2014-09-21 08:12:27 +0800

    Fix: fix bugs in member manager page

--- wechat_platform/api/member/serializer.py
+++ wechat_platform/api/member/serializer.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 
 from rest_framework import serializers
 
 from lib.tools import validator
 
 
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ('id', 'name', 'codename')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    permissions = PermissionSerializer()
+
+    class Meta:
+        model = Group
+        fields = ('id', 'name', 'permissions')
+
+
 class MemberSerializer(serializers.ModelSerializer):
-    nickname = serializers.SerializerMethodField('get_nickname')
-    group = serializers.SerializerMethodField('get_group')
-
-    def get_nickname(self, obj):
-        """
-        Just for test
-        :param obj:
-        :return:
-        """
-        return obj.username
-
-    def get_group(self, obj):
-        return u'超级管理员'
+    groups = GroupSerializer()
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'nickname', 'email', 'group', 'date_joined')
+        fields = ('id', 'username', 'nickname', 'email', 'groups', 'date_joined')
