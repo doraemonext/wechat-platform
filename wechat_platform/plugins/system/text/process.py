ec6d6824b965f9706aa5abfb1eb54f10c409ce79
doraemonext <doraemonext@gmail.com>
2014-11-06 14:15:08 +0800

    Add: add the log for system plugin

--- wechat_platform/plugins/system/text/process.py
+++ wechat_platform/plugins/system/text/process.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from django.core.exceptions import ObjectDoesNotExist
 
 from system.core.exceptions import PluginRuntimeError
 from system.plugin.framework import PluginProcessorSystem
 from system.library.text.models import LibraryText
+
+logger_plugin = logging.getLogger(__name__)
 
 __all__ = ['PluginSystemText']
 
@@ -17,6 +21,7 @@ class PluginSystemText(PluginProcessorSystem):
         try:
             content = LibraryText.objects.get(pk=self.reply_id)
         except ObjectDoesNotExist:
-            raise PluginRuntimeError('no reply id found when get content from text library')
+            logger_plugin.warning('No reply id found when get content from text library [ReplyID] %s' % self.reply_id)
+            raise PluginRuntimeError('No reply id found when get content from text library')
 
         return self.response_text(text=content)
