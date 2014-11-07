ccbec53068691be3404ef33aa7c1371f19f9b94f
doraemonext <doraemonext@gmail.com>
2014-11-07 17:51:05 +0800

    Add: finsh the simulation match method

--- wechat_platform/plugins/system/text/process.py
+++ wechat_platform/plugins/system/text/process.py
@@ -8,7 +8,7 @@ from system.plugin import PluginRuntimeError
 from system.plugin.framework import PluginProcessorSystem
 from system.library.text.models import LibraryText
 
-logger_plugin = logging.getLogger(__name__)
+logger_plugins = logging.getLogger(__name__)
 
 __all__ = ['PluginSystemText']
 
@@ -21,7 +21,7 @@ class PluginSystemText(PluginProcessorSystem):
         try:
             content = LibraryText.objects.get(pk=self.reply_id)
         except ObjectDoesNotExist:
-            logger_plugin.warning('No reply id found when get content from text library [ReplyID] %s' % self.reply_id)
+            logger_plugins.warning('No reply id found when get content from text library [ReplyID] %s' % self.reply_id)
             raise PluginRuntimeError('No reply id found when get content from text library')
 
         return self.response_text(text=content.content)
