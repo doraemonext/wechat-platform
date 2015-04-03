1cecb92f0ac621518c9b58033266d674a7082878
doraemonext <doraemonext@gmail.com>
2015-04-03 12:23:45 +0800

    TEST

--- wechat_platform/plugins/test/process.py
+++ wechat_platform/plugins/test/process.py
@@ -14,4 +14,9 @@ class PluginTest(PluginProcessor):
     测试插件
     """
     def process(self):
-        return self.response_text_basic(text='你好')
+        return self.wechat.send_template_message(
+            user_id=self.message.source,
+            template_id='HaBqpxp0FBaKF9eq1fxV6SpZTY0vV4I1Mm8I1pd-4WE',
+            data={},
+            url='http://www.baidu.com',
+        )
